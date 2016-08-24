2ad3a70fc4125ed246dfa1fb5fc342b0d314e2f4
piotrq18 <piotrq18@gmail.com>
2016-08-24 15:29:17 +0200

    Prep repo for heroku

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next){
-  if(req.headers['x-forwarded-proto'] = 'https'){
-    next();
-  }else{
-    res.redirect('http://' + req.hostname + req.url);
-  }
+app.use(function (req, res, next) {
+//  if (req.protocol                     === 'https') { // DOES NOT (reliably ??) WORK
+    if (req.headers['x-forwarded-proto'] === 'https') { // WORKED
+      console.log("WR__ 00 HTTP *S* Inside IF : req.protocol : "  + req.protocol);
+      console.log("WR__ 01 HTTP *S* Inside IF : req.headers['x-forwarded-proto']"  + req.headers['x-forwarded-proto']);
+      res.redirect('http://' + req.hostname + req.url);
+    } else {
+      next();
+   }
 });
 
 app.use(express.static('public'));
