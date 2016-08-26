43c43aae0af94f1f0c1810e21086e015d6bc4ec1
piotrq18 <piotrq18@gmail.com>
2016-08-26 02:27:55 +0200

    fixing issues and polishing app

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -13,7 +13,13 @@ var Weather = React.createClass({
     }
   },
   handleSearch: function(location){
-    this.setState({isLoading: true});
+    this.setState({
+      isLoading: true,
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
+    });
+
     var that = this;
     openWeatherMap.getTemp(location).then(function(temp){
       that.setState({
@@ -27,6 +33,22 @@ var Weather = React.createClass({
         errorMessage: e.message
       });
     })
+  },
+  componentDidMount: function(){
+    var location = this.props.location.query.location;
+
+    if(location && location.length > 0){
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function(newProps){
+    var location = newProps.location.query.location;
+
+    if(location && location.length > 0){
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
   },
   render: function(){
     var {isLoading, temp, location, errorMessage} = this.state;
